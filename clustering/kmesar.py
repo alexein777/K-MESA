d645133aa43f4a0e5be29da32871c07256712e46
alexein777 <a.mladenovic96@gmail.com>
2020-08-09 22:07:28 +0200

    K-Means standard version, iris tested

--- clustering/kmesar.py
+++ clustering/kmesar.py
@@ -1,6 +1,7 @@
 import numpy as np
 import numpy.linalg as la
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def euclidean_distance(vec1, vec2):
@@ -93,7 +94,7 @@ def initialize_centroids_random(points, k_centroids, lower_bound=None, upper_bou
     """
 
     n = points.shape[1]
-    centroids = np.zeros((k_centroids, n))
+    centroids = np.zeros(shape=(k_centroids, n))
 
     if lower_bound is None or upper_bound is None:
         for k in range(k_centroids):
@@ -102,5 +103,91 @@ def initialize_centroids_random(points, k_centroids, lower_bound=None, upper_bou
         for k in range(k_centroids):
             centroids[k] = lower_bound + np.random.random_sample((n,)) * (upper_bound - lower_bound)
 
+    # If there is a centroid with none points assigned to it, reinitialize one of centroids
+    n_labels = len(np.unique(assign_points_to_centroids(points, centroids)))
+
+    while n_labels < k_centroids:
+        rand_index = np.random.randint(0, k_centroids)
+
+        if lower_bound is None or upper_bound is None:
+            centroids[rand_index] = np.random.random_sample((n,))
+        elif lower_bound is not None and upper_bound is not None:
+            centroids[rand_index] = lower_bound + np.random.random_sample((n,)) * (upper_bound - lower_bound)
+
+        n_labels = len(np.unique(assign_points_to_centroids(points, centroids)))
+
     return centroids
+
+
+def check_centroids_update(old_centroids, new_centroids, tol, norm='euclidean'):
+    """
+    :param old_centroids: Cluster centroids from previous iteration: numpy array of shape (k, n)
+    :param new_centroids: Cluster centroids from current iteration: numpy array of shape (k, n)
+    :param tol: Tolerance parameter: real number
+    :return: K-Means stopping criterion: boolean
+    """
+
+    if norm == 'euclidean':
+        diff = la.norm(old_centroids - new_centroids, ord=2)
+    elif norm == 'frob':
+        diff = la.norm(old_centroids - new_centroids, ord='frob')
+    else:
+        raise ValueError(f'Unknown norm type: {norm}')
+
+    stoppping_criterion_reached = (diff <= tol).all()
+
+    return stoppping_criterion_reached
+
+
+class KMeansStandard:
+    def __init__(self, k_clusters=5, init='random', n_init=10, max_iter=300, tol=1e-4):
+        self.k_clusters = k_clusters
+        self.init = init
+        self.n_init = n_init
+        self.max_iter = max_iter
+        self.tol = tol
+
+        self.labels_ = None
+        self.centroids_ = None
+        self.intertia_ = None
+        self.n_iter_ = None
+        self.history_ = None
+
+    def fit(self, points):
+        lower_bound = np.min(points, axis=0)
+        upper_bound = np.max(points, axis=0)
+        history = {
+            'labels': [],
+            'centroids': [],
+            'inertia': [],
+            'n_iter': []
+        }
+
+        if type(points) == pd.DataFrame:
+            points = np.array(points)
+
+        for n_it in range(self.n_init):
+            initial_centroids = initialize_centroids_random(points, self.k_clusters, lower_bound, upper_bound)
+            centroids = initial_centroids
+
+            for it in range(self.max_iter):
+                labels = assign_points_to_centroids(points, centroids)
+                new_centroids = update_centroids(points, centroids, labels)
+                stopping_criterion_reached = check_centroids_update(centroids, new_centroids, self.tol)
+                centroids = new_centroids
+
+                if stopping_criterion_reached:
+                    break
+
+            history['labels'].append(labels)
+            history['centroids'].append(centroids)
+            history['inertia'].append(sum_of_squared_error(points, centroids, labels))
+            history['n_iter'].append(it)
+
+        best_result_index = np.argmin(history['inertia'])
+        self.history_ = history
+        self.labels_ = history['labels'][best_result_index]
+        self.centroids_ = history['centroids'][best_result_index]
+        self.inertia_ = history['inertia'][best_result_index]
+        self.n_iter_ = history['n_iter'][best_result_index]
 
